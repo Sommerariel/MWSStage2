df9f5598ec2aafe5216cc46288018aebf8426927
Sommerariel <s.a.shearer@hotmail.com>
2018-08-21 20:36:15 -0600

    feat: add uglify, del, pump, browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 const gulp = require('gulp');
+const browserify = require('browserify');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const del = require('del');
 
 gulp.task('default', () =>
     gulp.src('css/**/*.css')
